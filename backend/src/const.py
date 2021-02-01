5630178319054783917160eb8dfbdab8de99ae88
AhmedHumk <77048689+AhmedHumk@users.noreply.github.com>
2021-02-01 08:58:09 +0200

    Add a new model to hold a constant variables that i will use

---
+++ backend/src/const.py
@@ -0,0 +1,17 @@
+# a constants that i will simply use
+# into the project
+'''auth zero domain'''
+AUTH0_DOMAIN = "ahumk.us.auth0.com"
+'''auth zero Algorithm'''
+ALGORITHM = ["RS256"]
+'''auth zero API AUDIENCE'''
+API_AUDIENCE = "coffeshop"
+'''error handlers that i will use'''
+Err_Unauth = 401
+Err_badrequest = 400
+Err_server = 500
+Err_unavailable = 503
+Err_Notfound = 404
+Err_NotAllowed = 405
+Err_NotProcessed = 422
+
