d1756433f3d0c70ce41329f0441a861d08b76fa8
AhmedHumk <77048689+AhmedHumk@users.noreply.github.com>
2021-02-01 08:53:22 +0200

    implemented All Todos

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -3,11 +3,9 @@ from flask import request, _request_ctx_stack
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
+import const
 
-
-AUTH0_DOMAIN = 'udacity-fsnd.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'dev'
+# my auth0 data is in const model
 
 ## AuthError Exception
 '''
@@ -19,33 +17,64 @@ class AuthError(Exception):
         self.error = error
         self.status_code = status_code
 
-
 ## Auth Header
-
-'''
-@TODO implement get_token_auth_header() method
-    it should attempt to get the header from the request
-        it should raise an AuthError if no header is present
-    it should attempt to split bearer and the token
-        it should raise an AuthError if the header is malformed
-    return the token part of the header
-'''
+        
 def get_token_auth_header():
-   raise Exception('Not Implemented')
+    # lets get our Authorization Token from the header
+    authorization_token = request.headers.get("Authorization", None)
+    # if we dont have a valid authorization_token
+    # then raise an error
+    if not authorization_token:
+        raise AuthError({
+            "code": "Authorization_header_is_empty",
+            "description": "No Authorization header included"
+            }, const.Err_Unauth)
+    # lets split our authorization_token and check
+    # if we have Bearer and token
+    split_auth_token = authorization_token.split()
+    # before we do anything lets check split_auth_token length
+    # we excpect to have a length equal 2
+    # if the length is smaller than 2 then raise an error
+    if len(split_auth_token) < 2:
+        raise AuthError({
+            "code": "invalid_authorization_length",
+            "description": "invalid Authorization header length"
+            }, const.Err_Unauth)
+    # if everything is fine lets set
+    # our Bearer and token variables
+    # [0] for Bearer - [1] for token
+    Bearer_val = split_auth_token[0]
+    token_val = split_auth_token[1]
+    # lets check our Bearer_val if it has
+    # a word of bearer
+    if Bearer_val.lower() != "bearer":
+        raise AuthError({
+            "code": "invalid_header_bearer",
+            "description": "the bearer provided is invalid"
+            }, const.Err_Unauth)
+    # if everything is fine then
+    # return the token_val
+    return token_val
 
-'''
-@TODO implement check_permissions(permission, payload) method
-    @INPUTS
-        permission: string permission (i.e. 'post:drink')
-        payload: decoded jwt payload
 
-    it should raise an AuthError if permissions are not included in the payload
-        !!NOTE check your RBAC settings in Auth0
-    it should raise an AuthError if the requested permission string is not in the payload permissions array
-    return true otherwise
-'''
+# check premissions
 def check_permissions(permission, payload):
-    raise Exception('Not Implemented')
+    # lets check if we have,
+    # permissions in our playload
+    if "permissions" not in payload:
+        raise AuthError({
+            "code": "invalid_playload",
+            "description": "permissions is not included"
+            }, const.Err_badrequest)
+
+    if permission not in payload['permissions']:
+        raise AuthError({
+            "code": "invalid_playload",
+            "description": "permissions is not included"
+            }, const.Err_badrequest)
+    # if everything is fine return true
+    return True
+    
 
 '''
 @TODO implement verify_decode_jwt(token) method
@@ -60,9 +89,71 @@ def check_permissions(permission, payload):
 
     !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
 '''
+# Verify jwt decode
 def verify_decode_jwt(token):
-    raise Exception('Not Implemented')
+    # fetch my RSA key from Auth0
+    # based on Identity and Authentication lesson
+    Auth0UrL = "https://{}/.well-known/jwks.json".format(const.AUTH0_DOMAIN)
+    getjsonData = urlopen(Auth0UrL)
+    # Read our fetched json data
+    data_json = json.loads(getjsonData.read())
+    header_verfication = jwt.get_unverified_header(token)
+    # lets Verfiy the header
+    if "kid" not in header_verfication:
+        raise AuthError({
+            "code": "invalid_header",
+            "description": "header is not included"
+            }, const.Err_Unauth)
+        
+    myrskey = {}
+    # lets loop throw data_json
+    for key in data_json["keys"]:
+        if key["kid"] == header_verfication["kid"]:
+            myrskey = {
+                "kty": key["kty"],
+                "kid": key["kid"],
+                "use": key["use"],
+                "n": key["n"],
+                "e": key["e"]
+                }
+    # if we dont have a key then
+    # raise Error
+    if not myrskey:
+        raise AuthError({
+            "code": "invalid_header",
+            "description": "header is not included"
+            }, const.Err_Unauth)
+    # if we have a key then
+    # lets try to decode our token
+    try:
+        payload = jwt.decode(
+            token,
+            myrskey,
+            algorithms=const.ALGORITHM,
+            audience=const.API_AUDIENCE,
+            issuer="https://{}/".format(const.AUTH0_DOMAIN)
+            )
+        return payload
+    
+    except jwt.ExpiredSignatureError:
+        raise AuthError({
+            "code": "invalid_token",
+            "description": "this token is invalid"
+            }, const.Err_Unauth)
+    except jwt.JWTClaimsError:
+        raise AuthError({
+            "code": "invalid_claims",
+            "description": "invalid claims"
+            }, const.Err_Unauth)
+    except Exception as E:
+        print(E)
+        raise AuthError({
+            "code": "invalid_header",
+            "description": "invalid header"
+            }, const.Err_Unauth)
 
+    
+        
 '''
 @TODO implement @requires_auth(permission) decorator method
     @INPUTS
