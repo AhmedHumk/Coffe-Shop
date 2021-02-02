92031d24cea985bf15435c1eedc5e2ff281aae34
AhmedHumk <77048689+AhmedHumk@users.noreply.github.com>
2021-02-02 05:55:02 +0200

    applied the review removed auth0.com

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  apiServerUrl: 'http://127.0.0.1:4000', // the running FLASK api server url
+  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'ahumk.us.auth0.com', // the auth0 domain prefix
+    url: 'ahumk.us', // the auth0 domain prefix
     audience: 'coffeshop', // the audience set for the auth0 app
     clientId: 'l63nH3g4NiYvBrBSRYvN3rgeIpltDJWP', // the client id generated for the auth0 app
     callbackURL: 'http://127.0.0.1:8100', // the base url of the running ionic application.
